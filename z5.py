eb9019cccff29be14c69c25ebac11c6b587fb500
listmen001 <za.ignatev@gmail.com>
2025-08-11 14:38:26 +0400

    z5

---
+++ z5.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def main():
+    try:
+        number = int(input("Введите число: "))
+        if is_prime(number):
+            print(f"{number} - простое число!")
+        else:
+            print(f"{number} - не простое число!")
+    except ValueError:
+        print("Введите целое число!")
+
+
+if __name__ == "__main__":
+    main()
